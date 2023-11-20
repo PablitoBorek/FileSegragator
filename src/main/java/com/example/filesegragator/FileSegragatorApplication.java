564bcd8b0ad225f1fad16b0366e54294cf01df3d
Paweł Borkowski <pawel.borkowski@hycom.pl>
2023-11-20 11:28:00 +0100

    Initial commit

---
+++ src/main/java/com/example/filesegragator/FileSegragatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.filesegragator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSegragatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSegragatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/filesegragator/FileSegragatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filesegragator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSegragatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
